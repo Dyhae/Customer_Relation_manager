44453a57931a437970133b6fe5e07c4ccc907804
doulabi <dyhaeoulabi@outlook.com>
2020-03-20 23:35:49 -0500

    Add validation to the customer model or table

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,9 @@
 class Customer < ApplicationRecord
+
+  validates :name,:surname, presence: true
+
+  validates_each :name, :surname do |record, attr, value|
+    record.errors.add(attr, 'must start with upper case') if value =~ /\A[a-z]/
+  end
+  
 end
