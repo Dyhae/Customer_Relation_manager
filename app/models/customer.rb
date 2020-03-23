a86e0e07e712d3763d50fc5950530996ff95d6bf
doulabi <dyhaeoulabi@outlook.com>
2020-03-23 15:32:37 -0500

    modified the partial and seed file

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,8 @@
 class Customer < ApplicationRecord
 
-  validates :name,:surname, presence: true
+  has_one_attached :image
+
+  validates :name, :surname, presence: true
 
   validates_each :name, :surname do |record, attr, value|
     record.errors.add(attr, 'must start with upper case') if value =~ /\A[a-z]/
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,21 +7,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'faker'
+# require 'faker'
 
 Customer.destroy_all
 
-NUMBER_OF_PEOPLE = 5
+# NUMBER_OF_PEOPLE = 5
 
-NUMBER_OF_PEOPLE.times do
-  customer = Customer.create(
-    name: Faker::Name.name,
-    surname: Faker::Name.last_name,
-    phone_number: Faker::PhoneNumber.phone_number,
-    email: Faker::Internet.email,
-    notes: Faker::Quotes::Shakespeare.romeo_and_juliet_quote
-  )
+# NUMBER_OF_PEOPLE.times do
+#   customer = Customer.create(
+#     name: Faker::Name.name,
+#     surname: Faker::Name.last_name,
+#     phone_number: Faker::PhoneNumber.phone_number,
+#     email: Faker::Internet.email,
+#     notes: Faker::Quotes::Shakespeare.romeo_and_juliet_quote
+#   )
 
-  customer.save
+#   customer.save
+# end
+if Rails.env.development?
+  AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 end
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
